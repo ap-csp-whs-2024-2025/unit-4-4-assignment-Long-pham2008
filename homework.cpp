98ba8a215251c4d795967d0d1b7dd889079fe24e
Long-pham2008 <phamhoanglong180108@gmail.com>
2025-01-06 21:36:43 +0000

    Hello

--- homework.cpp
+++ homework.cpp
@@ -8,9 +8,11 @@ int main()
     // Problem 1
     int x = 25;
     int y = 3;
+    int z = 10;
 
     // Operations 
-    std::cout << "X + y = " << x + y << std::endl;
+    // std::cout << "X + y = " << x + y << std::endl;
+    std::cout << "x + z = " << x + z << std::endl;
 
     return 0;
 }
